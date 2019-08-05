4c7f16e504c690dc7a35eacdc40668cfb1e58e77
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-05 22:30:04 +0900

    first commit

---
+++ NumbersGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumbersGame
+//
+//  Created by VERTEX24 on 2019/08/05.
+//  Copyright © 2019 VERTEX24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

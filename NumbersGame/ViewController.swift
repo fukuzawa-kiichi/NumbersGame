4ab9dbb36d536e6462b526cd57e21fd9ef4105a6
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-05 23:01:31 +0900

    紐付けした。

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // 前に書かれた数字を表示する
+    @IBOutlet weak var numberLabel: UILabel!
+    // 数字を入力するとこ
+    @IBOutlet weak var numTextField: UITextField!
+    // 過去の結果を表示する
+    @IBOutlet weak var resultTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // ボタンの関数
+    @IBAction func button(_ sender: UIButton) {
+    }
+    
 }
 
